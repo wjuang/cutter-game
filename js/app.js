63fc5d3c1eba688c6b5795554a864ad6f08ce597
wjuang <williamsjuang@gmail.com>
2021-08-11 19:26:16 -0400

    player sprite moves left and right

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ const canvas = document.querySelector('#playspace')
 canvas.width = innerWidth
 canvas.height = innerHeight
 const ctx = canvas.getContext('2d')
+//frame counter
+let frame = 0
 
 //player class
 class Player {
@@ -20,6 +22,12 @@ class Player {
   draw(){
     ctx.drawImage(this.link, this.x, this.y)
   }
+  moveLeft(){
+    this.x -= 8
+  }
+  moveRight(){
+    this.x += 8
+  }
 }
 
 //player attack class
@@ -32,8 +40,32 @@ class Attack {
   draw(){
     ctx.drawImage(this.link, this.x, this.y)
   }
-
 }
+
+//enemy class
+class Enemy {
+  constructor(x, y, link){
+    this.x = x
+    this.y = y
+    this.link = link
+  }
+  draw(){
+    ctx.drawImage(this.link, this.x, this.y)
+  }
+}
+
+//function to animate things
+function animate() {
+  requestAnimationFrame(animate)
+  frame++
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  player.draw()
+}
+
+//game initialization
+const player = new Player(innerWidth/2, innerHeight/2, document.querySelector('#player'))
+animate()
+
 
 //event listener for attack
 window.addEventListener('keydown', function(e){
@@ -50,6 +82,31 @@ window.addEventListener('keydown', function(e){
   }
 })
 
-//instance of game
-const player = new Player(innerWidth/2, innerHeight/2, document.querySelector('#player'))
-player.draw()
+//event listeners for player movement
+window.addEventListener('keydown', function(e){
+  if (e.keyCode == 65 || e.keyCode == 37){
+    e.preventDefault()
+    //move left
+    console.log('left')
+    player.moveLeft()
+    player.draw()
+  }
+})
+
+window.addEventListener('keydown', function(e){
+  if (e.keyCode == 68 || e.keyCode == 39){
+    e.preventDefault()
+    //move right
+    console.log('right')
+    player.moveRight()
+    player.draw()
+  }
+})
+
+window.addEventListener('keydown', function(e){
+  if (e.keyCode == 87 || e.keyCode == 38){
+    e.preventDefault
+    //jump
+    console.log('jump')
+  }
+})
