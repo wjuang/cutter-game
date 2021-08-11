0ae63c5fb96c1942b8505401e0b654cda2c389a2
wjuang <williamsjuang@gmail.com>
2021-08-11 18:58:48 -0400

    spacebar spawns attack sprite

--- js/app.js
+++ js/app.js
@@ -11,6 +11,10 @@ class Player {
     this.y = y
     //placeholder for image element?
     this.link = link
+    //need to know if flipped or not
+    this.flip = false
+    //attack cooldown
+    this.cooldown = false
   }
   //draw method for player
   draw(){
@@ -18,5 +22,34 @@ class Player {
   }
 }
 
-const player = new Player(100, 100, document.querySelector('#player'))
+//player attack class
+class Attack {
+  constructor(x, y, link){
+    this.x = x
+    this.y = y
+    this.link = link
+  }
+  draw(){
+    ctx.drawImage(this.link, this.x, this.y)
+  }
+
+}
+
+//event listener for attack
+window.addEventListener('keydown', function(e){
+  if (e.keyCode == 32 && e.target == document.body){
+    //stop spacebar scroll
+    e.preventDefault()
+    e.stopPropagation()
+
+    const attack = new Attack(player.x + 50, player.y, document.querySelector('#attack'))
+    attack.draw()
+    setTimeout(function(){
+      //delete attack here
+    }, 1000)
+  }
+})
+
+//instance of game
+const player = new Player(innerWidth/2, innerHeight/2, document.querySelector('#player'))
 player.draw()
